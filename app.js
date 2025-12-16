2264e603a0f9c8c6504cdeb09787f1f49c3e94f3
Alexandre Matiello <alexandrematiellokf@gmail.com>
2025-12-16 17:32:01 -0300

    Configurei a aplicação para fazer o deploy.

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/", indexRouter);
 
 // servidor
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${PORT}`);
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
